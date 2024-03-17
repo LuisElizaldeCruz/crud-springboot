c6fbc7d5d79c86d95a4e817fa1894e512518ac1a
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2024-03-16 21:48:30 -0600

    creacion de los bootones editar y eliminar

--- src/main/java/com/tcna/primeraweb/controller/PersonaController.java
+++ src/main/java/com/tcna/primeraweb/controller/PersonaController.java
@@ -5,10 +5,7 @@ import com.tcna.primeraweb.service.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,6 +33,25 @@ public class PersonaController {
     @PostMapping("/nueva")
     public String guardarNuevaPersona(@ModelAttribute Persona persona){
         personaService.crearPersona(persona);
+        return "redirect:/personas";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String mostrarFormularioEditarPersona(@PathVariable Long id, @ModelAttribute Persona persona, Model model){
+        model.addAttribute("persona", persona);
+        model.addAttribute("action", "/personas/editar/"+id);
+        return "formulario";
+    }
+
+    @PostMapping("/editar/{id}")
+    public String actualizarPersona(@PathVariable Long id, @ModelAttribute Persona persona){
+        personaService.actualizarPersona(id, persona);
+        return "redirect:/personas";
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminarPersona(@PathVariable  Long id){
+        personaService.eliminarPersona(id);
         return "redirect:/personas";
     }
 }
